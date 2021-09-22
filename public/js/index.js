375fd88251ba379e61f7798f6720e4336d6d02b2
Vashti <patrena@sbcglobal.net>
2021-09-22 05:02:37 -0700

    Deploying

--- public/js/index.js
+++ public/js/index.js
@@ -137,7 +137,7 @@ function sendTransaction(isAdding) {
   .catch(err => {
     // fetch failed, so save in indexed db
     // saveRecord(transaction);
-    saveRecord(formData);
+    saveRecord(record);
 
     // clear form
     nameEl.value = "";
