3b2a71144dba9bea242838e3592553055fe892af
Vashti <patrena@sbcglobal.net>
2021-09-22 08:19:29 -0700

    updated index.js 137-140

--- public/js/index.js
+++ public/js/index.js
@@ -136,8 +136,7 @@ function sendTransaction(isAdding) {
   })
   .catch(err => {
     // fetch failed, so save in indexed db
-    // saveRecord(transaction);
-    saveRecord(record);
+    saveRecord(transaction);
 
     // clear form
     nameEl.value = "";
